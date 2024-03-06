ba3418c82d1ce4951f995c036aeae0a156fab7b7
Matt Smith <matt.marcus.smith@gmail.com>
2024-03-06 12:36:27 -0600

    Update testimonial section

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -12,14 +12,14 @@ export const Data = [
         image: Image1,
         title: "Daniel Roy - Lead Application Engineer",
         relation: "Daniel was Matt's Senior and Developer Mentor",
-        description: "What impressed me most about Matt was his eagerness and ability to rapidly learn and apply new skills, especially in backend technologies with .NET. His quick grasp of these complex concepts and seamless integration into our backend development processes was truly commendable."
+        description: "I was impressed with Matt’s eagerness and ability to rapidly learn and apply new skills, especially in backend technologies with .NET. His quick grasp of these complex concepts and seamless integration into our backend development processes was commendable."
     },
     {
         id: 2,
         image: Image2,
         title: "Hannah Schodowski - Data Science Intern",
         relation: "Hannah worked with Matt on the same team",
-        description: "During my time on the software team, I encountered the challenge of transitioning into a field that wasn't my primary area of study. However, Matt's exceptional ability to convey fundamental software concepts in a clear and approachable manner greatly aided my understanding of the software industry.",
+        description: "While on the software team, I encountered the challenge of transitioning into a field that was not my primary area of expertise. However, Matt's exceptional ability to convey fundamental software concepts clearly aided in my understanding of the software industry.",
     },
     {
         id: 3,
@@ -32,21 +32,21 @@ export const Data = [
         id: 4,
         image: Image4,
         title: "Kevin Gray - Department Manager",
-        relation: "Kevin was Matt's Manger and Business processes Mentor",
-        description: "Matt is enthusiastic and motivated. I am confident that he will devote himself to a position in your organization with a high degree of diligence. He is a quick learner and has shown a passion and ability to deliver was is needed.",
+        relation: "Kevin was Matt's Manager and Business processes Mentor",
+        description: "Matt is enthusiastic and motivated. I am confident he will devote himself to a position in your organization with diligence. He is a quick learner and has shown a passion and ability to deliver what is needed. ",
     },
     {
         id: 5,
         image: Image5,
         title: "Josh Miller - Scrum Master ",
         relation: "Josh overseen Agile process for Matts team",
-        description: "Matt quickly adapted to our company culture, and is an effective communicator as I observed from my role as agile Master. He is quick to reach out or research unknowns to find clarity. I am confident Matt will exhibit the same within your organization. I highly recommend Matt Smith.",
+        description: "Matt quickly adapted to our company culture and is an effective communicator as I observed from my role as an agile Master. He is quick to research unknowns to find clarity. I am confident Matt will exhibit the same within your organization. I highly recommend Matt Smith.",
     },
     {
         id: 6,
         image: Image5,
         title: "Matthew Pasztor - Senior Architect",
         relation: "Matthew was the Senior Architect on Matts Team",
-        description: "I been an Engineer for 22 years, and I was very impressed with Matt's humble and eagerness to learn. Matt's adaptability was seen during the final deployment of his project. We made changes to the location of his data for our internal system. Matt reached out to experts on varies teams including myself to assist with the redeployment.",
+        description: "I have been an Engineer for 22 years, and I was overly impressed with Matt's humility and eagerness to learn. Matt's adaptability was seen during the final deployment of his project. We changed the location of his data for our internal system. Matt contacted experts on various teams including me to help with the redeployment. ",
     },
 ]
